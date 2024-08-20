3855237f6f7dbfc7a15f42259dd15243df3738ce
zhangshuyu <zhangshuyu@haima.me>
2024-08-20 18:44:12 +0800

    es

--- quickstart-flink/quickstart-datastream/datastream_1.13/src/main/scala/org/apache/streampark/flink/quickstart/datastream/QuickStartApp.scala
+++ quickstart-flink/quickstart-datastream/datastream_1.13/src/main/scala/org/apache/streampark/flink/quickstart/datastream/QuickStartApp.scala
@@ -17,55 +17,44 @@
 
 package org.apache.streampark.flink.quickstart.datastream
 
+import org.apache.commons.lang3.StringUtils
 import org.apache.streampark.common.util.JsonUtils
-import org.apache.streampark.flink.connector.jdbc.sink.JdbcSink
+import org.apache.streampark.flink.connector.elasticsearch7.sink.ES7Sink
 import org.apache.streampark.flink.connector.kafka.source.KafkaSource
 import org.apache.streampark.flink.core.scala.FlinkStreaming
 import org.apache.flink.api.common.typeinfo.TypeInformation
+import org.apache.streampark.flink.connector.elasticsearch7.util.ElasticsearchUtils
+import org.elasticsearch.action.index.IndexRequest
+import org.json4s.DefaultFormats
+import org.json4s.jackson.Serialization
+
+import scala.language.implicitConversions
 
 object QuickStartApp extends FlinkStreaming {
 
   implicit val stringType: TypeInformation[String] = TypeInformation.of(classOf[String])
-  implicit val userType: TypeInformation[User] = TypeInformation.of(classOf[User])
+  implicit val logType: TypeInformation[Log] = TypeInformation.of(classOf[Log])
+  implicit lazy val formats: DefaultFormats.type = org.json4s.DefaultFormats
 
-  /**
-   * 假如我们用从kafka里读取用户的数据写入到mysql中,需求如下<br/>
-   *
-   * <strong>1.从kafka读取用户数据写入到mysql</strong>
-   *
-   * <strong>2. 只要年龄小于30岁的数据</strong>
-   *
-   * <strong>3. kafka数据格式如下:</strong>
-   * {
-   * "name": "benjobs",
-   * "age":  "28",
-   * "gender":   "1",
-   * "address":  "beijing"
-   * }
-   *
-   * <strong>4. mysql表DDL如下:</strong>
-   * create table user(
-   * `name` varchar(32),
-   * `age` int(3),
-   * `gender` int(1),
-   * `address` varchar(255)
-   * )
-   */
   override def handle(): Unit = {
     val source = KafkaSource()
       .getDataStream[String]()
-      .map(x => JsonUtils.read[User](x.value))
-      .filter(_.age < 30)
+      .map(x => JsonUtils.read[Log](x.value))
+//      .filter(l => StringUtils.isNotBlank(l.message))
+//      .map{log =>
+//        val map = JsonUtils.read[Map[String, Any]](JsonUtils.write(log))
+//        val strings = log.message.split("\\s")
+//        map
+//
+//      }
 
-    JdbcSink().sink[User](source)(user =>
-      s"""
-         |insert into t_user(`name`,`age`,`gender`,`address`)
-         |value('${user.name}',${user.age},${user.gender},'${user.address}')
-         |""".stripMargin
-    )
+
+    implicit def indexReq(x: Log): IndexRequest = ElasticsearchUtils.indexRequest(index = "stream_park_test", id = "_doc", source = Serialization.write(x))
+
+    ES7Sink().sink(source)
   }
 
 }
 
-case class User(name: String, age: Int, gender: Int, address: String)
+case class Log(hostname: String, namespace: String, log_type: String, message: String, `@timestamp`: String) extends Serializable
 
