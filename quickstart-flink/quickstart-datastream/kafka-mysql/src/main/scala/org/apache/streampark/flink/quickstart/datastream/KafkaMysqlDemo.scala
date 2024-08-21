741430155dbae7e0bdb81a550c7810c29a4d3adf
zhangshuyu <zhangshuyu@haima.me>
2024-08-21 11:23:55 +0800

    stream kafka-mysql

---
+++ quickstart-flink/quickstart-datastream/kafka-mysql/src/main/scala/org/apache/streampark/flink/quickstart/datastream/KafkaMysqlDemo.scala
@@ -0,0 +1,48 @@
+
+package org.apache.streampark.flink.quickstart.datastream
+
+import org.apache.commons.lang3.StringUtils
+import org.apache.streampark.common.util.JsonUtils
+import org.apache.streampark.flink.connector.kafka.source.KafkaSource
+import org.apache.streampark.flink.core.scala.FlinkStreaming
+import org.apache.flink.api.common.typeinfo.TypeInformation
+import org.apache.streampark.flink.connector.jdbc.sink.JdbcSink
+import org.json4s.DefaultFormats
+
+import scala.language.implicitConversions
+
+object KafkaMysqlDemo extends FlinkStreaming {
+
+  implicit val stringType: TypeInformation[String] = TypeInformation.of(classOf[String])
+  implicit val logType: TypeInformation[Log] = TypeInformation.of(classOf[Log])
+  implicit lazy val formats: DefaultFormats.type = org.json4s.DefaultFormats
+
+  override def handle(): Unit = {
+    val source = KafkaSource()
+      .getDataStream[String]()
+      .map(x => JsonUtils.read[Log](x.value))
+      .filter(l => StringUtils.isNotBlank(l.message))
+      .map { log =>
+        val map: Map[String, String] = log.message.split("\\s").filter(s => s.contains(":")).map(s => {
+          val strings: Array[String] = s.split(":", 2)
+          val key: String = strings(0)
+          val value: String = strings(1).replaceAll("'", "")
+          (key, value)
+        }).toMap
+        val cid = map.getOrElse("cid", "0")
+        log.copy(cid = cid)
+      }
+
+
+    JdbcSink().sink[Log](source)(log =>
+      s"""
+         |insert into demo_log(`hostname`,`namespace`,`log_type`,`cid`,`message`,`timestamp`)
+         |value('${log.hostname}','${log.namespace}','${log.log_type}','${log.cid}',,'${log.message}','${log.`@timestamp`}')
+         |""".stripMargin
+    )
+  }
+
+}
+
+case class Log(hostname: String, namespace: String, log_type: String, cid: String, message: String, `@timestamp`: String) extends Serializable
+
